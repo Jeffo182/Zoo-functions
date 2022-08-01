32d105669fc73b59f3588574aceea9533945eae7
Jefferson Felix <jefferson.felix182@gmail.com>
2022-08-01 18:23:32 -0300

    correcao 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,31 +1,32 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const { age } = entrants;
   const resultado = {
     child: 0,
     adult: 0,
     senior: 0,
   };
-
-  if (age < 18) {
-    resultado.child += 1;
-  }
-  if (age >= 18 && age < 50) {
-    resultado.adult += 1;
-  }
-  if (age >= 50) {
-    resultado.senior += 1;
-  }
+  entrants.forEach((element) => {
+    const { age } = element;
+    if (age < 18) {
+      resultado.child += 1;
+    }
+    if (age >= 18 && age < 50) {
+      resultado.adult += 1;
+    }
+    if (age >= 50) {
+      resultado.senior += 1;
+    }
+  });
   return resultado;
 }
 
 function calculateEntry(entrants) {
-  if (entrants === 0 || entrants === undefined) {
+  if (!entrants || Object.keys(entrants).length === 0 || entrants === undefined) {
     return 0;
   }
-  countEntrants(entrants);
-  const { child, adult, senior } = entrants;
+  const totalEntradas = countEntrants(entrants);
+  const { child, adult, senior } = totalEntradas;
   const resultado = data.prices.adult * adult
     + data.prices.child * child
     + data.prices.senior * senior;
